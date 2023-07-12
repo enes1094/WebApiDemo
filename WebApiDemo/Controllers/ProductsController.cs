1f4e36c161fc15032132a878446427fe151ac3cb
enes1094 <enes.tikici123@hotmail.com>
2023-07-12 18:53:26 +0300

    Added Complex Typs

--- WebApiDemo/Controllers/ProductsController.cs
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -3,16 +3,105 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiDemo.DataAccess;
+using WebApiDemo.Entities;
 
 namespace WebApiDemo.Controllers
 {
     [Route("api/products")]
-    public class ProductsController:Controller
+    public class ProductsController : Controller
     {
-        [HttpGet("")]
-        public string Get()
+        IProductDal _productDal;
+
+
+        public ProductsController(IProductDal productDal)
         {
-            return "Product1";
+            _productDal = productDal;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var products = _productDal.GetList();
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public IActionResult Get(int productId)
+        {
+            try
+            {
+                var product = _productDal.Get(p => p.ProductId == productId);
+                if (product == null)
+                {
+                    return NotFound($"There is no product with Id = {productId}");
+                }
+                return Ok(product);
+            }
+            catch { }
+
+            return BadRequest();
+        }
+
+        public IActionResult Post(Product product)
+        {
+            try
+            {
+                _productDal.Add(product);
+                return new StatusCodeResult(201);
+            }
+            catch
+            {
+
+
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Product product)
+        {
+            try
+            {
+                _productDal.Update(product);
+                return Ok(product);
+            }
+            catch
+            {
+
+
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{productId}")]
+        public IActionResult Delete(int productId)
+        {
+            try
+            {
+                _productDal.Delete(new Product { ProductId = productId);
+                return Ok();
+            }
+            catch
+            {
+
+
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("GetProductDetails")]
+        public IActionResult GetProductsWithDetails()
+        {
+            try
+            {
+                var result = _productDal.GetProductsWithDetails();
+                return Ok(result);
+            }
+            catch
+            {
+            }
+            return BadRequest();
         }
     }
 }
